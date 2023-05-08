25cd51d285be933e6beb272e1979e27e7b67362f
odashi <odashi@inspiredco.ai>
2023-05-08 23:14:06 +0000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+module.exports = {
+  devServer: {
+    static: { directory: path.join(__dirname, "dist") },
+    port: 3000,
+  },
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["@babel/preset-env", "@babel/react"] },
+          },
+          {
+            loader: "ts-loader",
+            options: { configFile: path.resolve(__dirname, "tsconfig.json") },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  target: "web",
+};
